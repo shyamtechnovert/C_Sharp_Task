e6213c6c2e5bcb15916e5e6645f842eb49d29b41
S.Shyam Sundar <113343639+shyamtechnovert@users.noreply.github.com>
2023-01-18 15:03:05 +0530

    Initial Commit

---
+++ Parking lot/Program.cs
@@ -0,0 +1,59 @@
+﻿
+using Parking_lot;
+using Parking_lot.Services;
+using System.Net.Sockets;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Initializing a parking lot.\n");
+
+        Console.WriteLine("No of spaces to be allotted for Two Wheelers : ");
+        int twoWheelerLimit = Convert.ToInt16(Console.ReadLine());
+
+        Console.WriteLine("No of spaces to be allotted for Four Wheelers : ");
+        int fourWheelerLimit = Convert.ToInt16(Console.ReadLine());
+
+        Console.WriteLine("No of spaces to be allotted for Heavy Vehicles : ");
+        int heavyWheelerLimit = Convert.ToInt16(Console.ReadLine());
+
+        ParkingOperations Operations = new ParkingOperations(twoWheelerLimit,fourWheelerLimit,heavyWheelerLimit);
+         
+        Console.WriteLine("Parking Lot Menu - \n");
+
+        while (true)
+        {
+
+            Console.WriteLine("1. See Parking Lot current occupancy details. \n2. Park Vehicle and Issue Ticket. \n3. Un-park Vehicle. \n4. History of Tickets \n5. Exit");
+            Console.Write("Your Choice :  ");
+            int SelectedMenu = Convert.ToInt16(Console.ReadLine());
+            switch (SelectedMenu)
+            {
+                case 1:
+                    Operations.SlotDetails();
+                    break;
+
+                case 2:
+                    Operations.ParkVehicle();
+                    break;
+
+                case 3:
+                    Operations.UnPackVehicle();
+                    break;
+                case 4:
+                    Operations.HistoryOfTickets();
+                    break;
+                case 5:
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("-----Invalid Selection-----");
+                    Console.WriteLine();
+                    break;
+            }
+        }
+    }
+
+        
+}
